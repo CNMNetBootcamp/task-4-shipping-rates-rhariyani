037d3bffba06be54dfb3a8fdff99f891ca2f78df
rishita.hariyani@gmail.com <rishita.hariyani@gmail.com>
2019-01-19 21:17:06 -0700

    final comit

---
+++ Task4/Pseudocode.cs
@@ -0,0 +1,40 @@
+//set up
+
+double packageweigt = 0.0;
+double cost = 0.0;
+
+
+
+// input
+
+    "Please enter packageweight?"
+
+
+//process
+
+cost = packageweight* rate // i have directly used price in numeric to condition;
+
+
+    if(packageweight<=2);
+    {
+       cost= packagewwight*2;
+    }
+
+    else if(packageweight<=5);
+    {
+          cost= packagewwight*4;
+    }
+
+    else if(packageweight<=10);
+cost= packagewwight*6;
+
+    else 
+cost= packagewwight*8;
+
+//output
+
+    put "This is the Cost to ship your package:" + cost;
+
+
+    // I know there is complex method to calculate shipping .
+    // Do i need to use otherway or this simple way is fine?
--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -6,7 +6,32 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double packageweigt = 0.0;
+            double cost = 0.0;
+
+            Console.WriteLine("Please enter your package weight:");
+            packageweigt = double.Parse(Console.ReadLine());
+
+            if(packageweigt <=2)
+            {
+                cost = packageweigt * 2;
+            }
+            else if (packageweigt <= 5)
+            {
+                cost = packageweigt * 4;
+            
+            }
+            else if(packageweigt <= 10)
+            {
+                cost = packageweigt * 6;
+            
+            }
+            else
+            {
+                cost = packageweigt * 8;
+            }
+            Console.WriteLine("The cost for shipping your package is:" + cost);
+            Console.ReadLine();
         }
     }
 }
